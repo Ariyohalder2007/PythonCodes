c9a13e1810046e4c3f6a98852f19fa83d5c12ce3
Ariyo <alokhalder6359@gmail.com>
2020-09-19 17:06:17 +0530

    set practice

--- prac5.py
+++ prac5.py
@@ -1,7 +1,9 @@
 l=['Souvick Mitra ', 'Ariyo Halder', 'Meghali Das']
 for i in l:
-    print(i[0], end=".")
-    for j in range(0, len(i)):
-        if i[j]==' ':
-            print(i[j+1])
+    # print(i[0], end=".")
+    # for j in range(0, len(i)):
+    #     if i[j]==' ':
+    #         print(i[j+1])
+    s=i.split(' ')
+    print(s[0][0],'.', s[1][0])
         
