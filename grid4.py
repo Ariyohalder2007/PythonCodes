a309dab48a290bc7af57295fec0ab41986c8a208
Ariyo <alokhalder6359@gmail.com>
2020-09-22 16:19:42 +0530

    Minnor Bug fix

--- grid4.py
+++ grid4.py
@@ -6,6 +6,5 @@ for i in range(1, n+1):
         print(j, end="")
     for j in range(i-1, 0, -1):
         print(j, end="")
-    for k in range(n-i, 0, -1):
-        print(" ", end='')
+    
     print()
