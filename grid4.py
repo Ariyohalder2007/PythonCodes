c41ed24a750dbe651d92f202b68fc1ca0d3d6384
Ariyo <alokhalder6359@gmail.com>
2020-09-22 16:11:14 +0530

    grid practice

---
+++ grid4.py
@@ -0,0 +1,11 @@
+n=4
+for i in range(1, n+1):
+    for k in range(n-i, 0, -1):
+        print(" ", end='')
+    for j in range(1, i+1):
+        print(j, end="")
+    for j in range(i-1, 0, -1):
+        print(j, end="")
+    for k in range(n-i, 0, -1):
+        print(" ", end='')
+    print()
