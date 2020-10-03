7c0d16f2c3554104d1a17caa539f453277e10223
Ariyo <alokhalder6359@gmail.com>
2020-10-03 17:27:09 +0530

    linked list

---
+++ linked-list.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data=None):
+        self.data=data
+        self.nextval=None
+
+class SLinkedList:
+    def __init__(self):
+        self.headval=None
+    def display(self):
+        printval=self.headval
+        while printval is not None:
+            print(printval.data, end=" ")
+            printval=printval.nextval
+    def insertAtbeginning(self,newdata):
+        newnode=Node(newdata)
+        newnode.nextval=self.headval
+        self.headval=newnode
+
+
+
+s=SLinkedList()
+s.headval=Node(3)
+a=Node(4)
+b=Node(5)
+s.headval.nextval=a
+a.nextval=b
+s.insertAtbeginning(2)
+s.display()
